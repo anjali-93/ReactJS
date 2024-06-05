00c4db1c346100e96a76298170b487c5f913b91c
Anjali Saxena <anjalisaxena667@gmail.com>
2024-06-05 12:43:44 +0530

    final commit

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <>
-      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
+      <div className='w-full max-w-lg mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800'>
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
           <input
@@ -54,7 +54,7 @@ function App() {
             onClick={copyPasswordToClip}
             className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy </button>
         </div>
-        <div className='flex text-sm gap-x-1'>
+        <div className='flex text-sm gap-x-3'>
           <div className='flex items-center gap-x-1'>
             <input
               type="range"
