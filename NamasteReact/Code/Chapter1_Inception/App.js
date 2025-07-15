551d534f97dc1df2fa8b80a2463b28aafc30d811
Anjali Saxena <anjalisaxena667@gmail.com>
2025-07-15 21:26:42 +0530

    today work

---
+++ NamasteReact/Code/Chapter1_Inception/App.js
@@ -0,0 +1,5 @@
+  const heading = React.createElement("h1", {}, "Hello World! Using React");
+    //  Get the root Element
+     const root = ReactDOM.createRoot(document.getElementById("root"));
+    //  Render the heading to the root Element
+     root.render(heading);
